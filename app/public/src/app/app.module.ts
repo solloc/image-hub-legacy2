663cbc060e06258fb1cb4cb4ae0d25820a4c0673
solloc <matthias@maisch.me>
2017-04-09 15:50:05 +0200

    Included bootstrap. Set up basic routing

--- app/public/src/app/app.module.ts
+++ app/public/src/app/app.module.ts
@@ -2,17 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' }
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('./public/dist/index', { title: 'Express' });
-    res.sendfile('./public/dist/index.html');
+    res.sendFile('./public/dist/index.html');
 });
 
 module.exports = router;
